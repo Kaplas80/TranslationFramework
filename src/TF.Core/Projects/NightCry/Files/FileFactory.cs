3861d4d7d8bba41ebef163f0e9ca6f009df476bb
kaplas <kaplas80@gmail.com>
2018-11-29 19:29:26 +0100

    Con esto puedo editar los textos, pero hace falta crear una fuente con el BMFont

---
+++ src/TF.Core/Projects/NightCry/Files/FileFactory.cs
@@ -0,0 +1,27 @@
+﻿using TF.Core.Entities;
+
+namespace TF.Core.Projects.NightCry.Files
+{
+    public static class FileFactory
+    {
+        public static ITFFile GetFile(string fileName)
+        {
+            if (fileName.EndsWith(".txt"))
+            {
+                return new TxtFile(fileName);
+            }
+
+            return null;
+        }
+
+        public static ITFFile GetFile(string fileName, byte[] fileContent)
+        {
+            if (fileName.EndsWith(".txt"))
+            {
+                return new TxtFile(fileName);
+            }
+
+            return null;
+        }
+    }
+}
