a6be3dcaf997dfd1de863566f667bdb7f72126e4
kaplas <kaplas80@gmail.com>
2018-12-04 13:31:36 +0100

    Corrección msg Yakuza 0

--- src/TF.Core/Projects/Yakuza0/Files/MsgFile.cs
+++ src/TF.Core/Projects/Yakuza0/Files/MsgFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -72,7 +73,7 @@ namespace TF.Core.Projects.Yakuza0.Files
         {
             s.ReadBytes(3);
             var num1 = s.ReadValueS8();
-            var headerLength = num1 * 16 + 28;
+            var headerLength = num1 * 16 + 24;
 
             s.Seek(0, SeekOrigin.Begin);
 
@@ -84,18 +85,21 @@ namespace TF.Core.Projects.Yakuza0.Files
             _dataList = new List<DataItem>();
             _totalStringLength = 0;
 
+            var pointer1 = (_header[8] << 24) + (_header[9] << 16) + (_header[10] << 8) + _header[11];
+
+            s.Seek(4, SeekOrigin.Current);
             _firstStringOffset = s.ReadValueS32(Endianness);
             _firstPropertyOffset = s.ReadValueS32(Endianness);
-
-            s.Seek(-8, SeekOrigin.Current);
+            _totalStringLength = pointer1 - _firstStringOffset;
+            s.Seek(-12, SeekOrigin.Current);
 
             var start = s.Position;
             while (start < _firstPropertyOffset)
             {
                 var item = new DataItem();
+                item.unknown = s.ReadValueS32(Endianness);
                 var stringOffset = s.ReadValueS32(Endianness);
                 item.PropertiesOffset = s.ReadValueS32(Endianness);
-                item.unknown = s.ReadValueS32(Endianness);
 
                 var pos = s.Position;
                 
@@ -103,7 +107,7 @@ namespace TF.Core.Projects.Yakuza0.Files
                 var pauses = GetPauses(s);
 
                 s.Seek(stringOffset, SeekOrigin.Begin);
-                var str = GetString(s, start.ToString("X8"));
+                var str = GetString(s, (start + 4).ToString("X8"));
                 s.Seek(pos, SeekOrigin.Begin);
 
                 InsertPauses(str, pauses);
@@ -111,7 +115,7 @@ namespace TF.Core.Projects.Yakuza0.Files
                 item.Data = str;
                 _dataList.Add(item);
                 
-                _totalStringLength += GetLength(str.Original, Encoding, false, false) + 1;
+                //_totalStringLength += GetLength(str.Original, Encoding, false, false) + 1;
 
                 start = s.Position;
             }
@@ -217,6 +221,7 @@ namespace TF.Core.Projects.Yakuza0.Files
         private string RemoveTags(string strOriginal)
         {
             var temp = strOriginal.Replace("\\r\\n", " ");
+            temp = temp.Replace("\\n", " ");
             temp = Regex.Replace(temp, @"<Color[^>]*>", string.Empty);
             return Regex.Replace(temp, @"<[^>]*>", " ");
         }
@@ -312,83 +317,37 @@ namespace TF.Core.Projects.Yakuza0.Files
             using (var fs = new FileStream(fileName, FileMode.Create))
             {
                 WriteHeader(fs, strings, options);
-                WriteData(fs, strings, options);
-                WriteRemainder(fs, strings, options);
+                var pointer1 = WriteData(fs, strings, options);
+                var newPointers = WriteRemainder(fs, strings, options);
+
+                fs.Seek(8, SeekOrigin.Begin);
+                fs.WriteValueS32(pointer1, Endianness);
+                fs.Seek(4, SeekOrigin.Current);
+                fs.WriteValueS32(newPointers.Item1, Endianness);
+                fs.WriteValueS32(newPointers.Item2, Endianness);
             }
         }
 
         public void Save(Stream s, byte[] originalContent, IList<TFString> strings, ExportOptions options)
         {
             WriteHeader(s, strings, options);
-            WriteData(s, strings, options);
-            WriteRemainder(s, strings, options);
+
+            var pointer1 = WriteData(s, strings, options);
+            var newPointers = WriteRemainder(s, strings, options);
+
+            s.Seek(8, SeekOrigin.Begin);
+            s.WriteValueS32(pointer1, Endianness);
+            s.Seek(4, SeekOrigin.Current);
+            s.WriteValueS32(newPointers.Item1, Endianness);
+            s.WriteValueS32(newPointers.Item2, Endianness);
         }
 
         private void WriteHeader(Stream s, IList<TFString> strings, ExportOptions options)
         {
-            var newTotalStringLength = 0;
-            foreach (var item in _dataList)
-            {
-                var str = FindString(strings, item.Data.Offset);
-
-                if (str.Original == str.Translation)
-                {
-                    newTotalStringLength += GetLength(str.Original, Encoding, false, false) + 1;
-                }
-                else
-                {
-                    newTotalStringLength += GetLength(str.Translation, options.SelectedEncoding, false, false) + 1;
-                }
-            }
-            var dif = newTotalStringLength - _totalStringLength;
-
-            var newTotalNamesLength = 0;
-            foreach (var item in _namesList)
-            {
-                var str = FindString(strings, item.Offset);
-
-                if (str.Original == str.Translation)
-                {
-                    newTotalNamesLength += GetLength(str.Original, Encoding, false, false) + 1;
-                }
-                else
-                {
-                    newTotalNamesLength += GetLength(str.Translation, options.SelectedEncoding, false, false) + 1;
-                }
-            }
-            
-            var dif2 = newTotalNamesLength - _totalNamesLength;
-
-            var pointer1 = (_header[8] << 24) + (_header[9] << 16) + (_header[10] << 8) + _header[11];
-            var pointer2 = (_header[16] << 24) + (_header[17] << 16) + (_header[18] << 8) + _header[19];
-            var pointer3 = (_header[20] << 24) + (_header[21] << 16) + (_header[22] << 8) + _header[23];
-            
-            var newPointer1 = 0;
-            var newPointer2 = 0;
-            var newPointer3 = 0;
-            
-            if (pointer1 > 0)
-            {
-                newPointer1 = pointer1 + dif;
-            }
-            if (pointer2 > 0)
-            {
-                newPointer2 = pointer2 + dif;
-            }
-            if (pointer3 > 0)
-            {
-                newPointer3 = pointer3 + dif + dif2;
-            }
-
-            s.Write(_header, 0, 8);
-            s.WriteValueS32(newPointer1, Endianness);
-            s.Write(_header, 12, 4);
-            s.WriteValueS32(newPointer2, Endianness);
-            s.WriteValueS32(newPointer3, Endianness);
-            s.Write(_header, 24, _header.Length - 24);
+            s.WriteBytes(_header);
         }
 
-        private void WriteData(Stream s, IList<TFString> strings, ExportOptions options)
+        private int WriteData(Stream s, IList<TFString> strings, ExportOptions options)
         {
             var stringOffset = _firstStringOffset;
             var pos = s.Position;
@@ -399,9 +358,9 @@ namespace TF.Core.Projects.Yakuza0.Files
             s.Seek(pos, SeekOrigin.Begin);
             foreach (var value in _dataList)
             {
+                s.WriteValueS32(value.unknown, Endianness);
                 s.WriteValueS32(stringOffset, Endianness);
                 s.WriteValueS32(value.PropertiesOffset, Endianness);
-                s.WriteValueS32(value.unknown, Endianness);
 
                 pos = s.Position;
                 s.Seek(stringOffset, SeekOrigin.Begin);
@@ -438,10 +397,22 @@ namespace TF.Core.Projects.Yakuza0.Files
                     var newLength = GetLength(translationTuple.Item1, options.SelectedEncoding, true, true);
                     var originalLength = GetLength(originalTuple.Item1, Encoding, true, true);
                     UpdateLength(s, (short) originalLength, (short) newLength);
+
+                    s.Seek(pos - 12, SeekOrigin.Begin);
+                    s.WriteValueS16((short)newLength, Endianness);
                 }
 
                 s.Seek(pos, SeekOrigin.Begin);
             }
+
+            s.Seek(0, SeekOrigin.End);
+
+            while (s.Position % 8 != 0)
+            {
+                s.WriteByte(0);
+            }
+
+            return _section1 == null? 0:(int)s.Position;
         }
 
         private List<Tuple<short, short>> ParseTags(string input)
@@ -586,96 +557,48 @@ namespace TF.Core.Projects.Yakuza0.Files
             return new Tuple<string, IList<short>>(strResult, pauses);
         }
 
-        private void WriteRemainder(Stream s, IList<TFString> strings, ExportOptions options)
+        private Tuple<int, int> WriteRemainder(Stream s, IList<TFString> strings, ExportOptions options)
         {
-            var newTotalStringLength = 0;
-            foreach (var item in _dataList)
-            {
-                var str = FindString(strings, item.Data.Offset);
-
-                if (str.Original == str.Translation)
-                {
-                    newTotalStringLength += GetLength(str.Original, Encoding, false, false) + 1;
-                }
-                else
-                {
-                    newTotalStringLength += GetLength(str.Translation, options.SelectedEncoding, false, false) + 1;
-                }
-            }
-            var dif = newTotalStringLength - _totalStringLength;
-
-            var newTotalNamesLength = 0;
-            foreach (var item in _namesList)
-            {
-                var str = FindString(strings, item.Offset);
-
-                if (str.Original == str.Translation)
-                {
-                    newTotalNamesLength += GetLength(str.Original, Encoding, false, false) + 1;
-                }
-                else
-                {
-                    newTotalNamesLength += GetLength(str.Translation, options.SelectedEncoding, false, false) + 1;
-                }
-            }
-            
-            var dif2 = newTotalNamesLength - _totalNamesLength;
-
-            var pointer1 = (_header[8] << 24) + (_header[9] << 16) + (_header[10] << 8) + _header[11];
-            var pointer2 = (_header[16] << 24) + (_header[17] << 16) + (_header[18] << 8) + _header[19];
-            var pointer3 = (_header[20] << 24) + (_header[21] << 16) + (_header[22] << 8) + _header[23];
-            
-            var newPointer1 = 0;
-            var newPointer2 = 0;
-            var newPointer3 = 0;
-            
-            if (pointer1 > 0)
-            {
-                newPointer1 = pointer1 + dif;
-            }
-            if (pointer2 > 0)
-            {
-                newPointer2 = pointer2 + dif;
-            }
-            if (pointer3 > 0)
-            {
-                newPointer3 = pointer3 + dif + dif2;
-            }
-
-            var section1Size = ((_header[12] << 8) + _header[13]) * 16;
             var section2Count = ((_header[14] << 8) + _header[15]);
 
-            if (newPointer1 > 0)
+            if (_section1 != null)
             {
-                s.Seek(newPointer1, SeekOrigin.Begin);
                 s.WriteBytes(_section1);
             }
 
-            if (newPointer2 > 0)
-            {
-                s.Seek(newPointer2, SeekOrigin.Begin);
-                var stringOffset = newPointer2 + section2Count * 4;
-                for (var i = 0; i < section2Count; i++)
-                {
-                    s.WriteValueS32(stringOffset, Endianness);
-                    
-                    var pos = s.Position;
-                    s.Seek(stringOffset, SeekOrigin.Begin);
-                    
-                    var str = FindString(strings, _namesList[i].Offset);
-                    WriteString(s, str, options);
+            var newPointer2 = (int) s.Position;
 
-                    stringOffset = (int) s.Position;
-                    
-                    s.Seek(pos, SeekOrigin.Begin);
-                }
+            var stringOffset = newPointer2 + section2Count * 4;
+            for (var i = 0; i < section2Count; i++)
+            {
+                s.WriteValueS32(stringOffset, Endianness);
+                
+                var pos = s.Position;
+                s.Seek(stringOffset, SeekOrigin.Begin);
+                
+                var str = FindString(strings, _namesList[i].Offset);
+                WriteString(s, str, options);
+
+                stringOffset = (int) s.Position;
+                
+                s.Seek(pos, SeekOrigin.Begin);
             }
 
-            if (newPointer3 > 0)
+            if (section2Count == 0)
             {
-                s.Seek(newPointer3, SeekOrigin.Begin);
+                newPointer2 = 0;
+            }
+
+            s.Seek(0, SeekOrigin.End);
+            var newPointer3 = 0;
+            
+            if (_section3 != null)
+            {
+                newPointer3 = (int)s.Position;
                 s.WriteBytes(_section3);
             }
+
+            return new Tuple<int, int>(newPointer2, newPointer3);
         }
 
         private static TFString FindString(IEnumerable<TFString> strings, int offset)
---
+++ src/TF.Core/Projects/Yakuza0/Files/MsgFileNew.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Gibbed.IO;
+using TF.Core.Entities;
+
+namespace TF.Core.Projects.Yakuza0.Files
+{
+    public class MsgFileNew : TFFile
+    {
+        private class Property
+        {
+            public ushort Type { get; set; }
+            public byte[] Unknown1 { get; set; }
+            public byte[] Unknown2 { get; set; }
+            public ushort Position { get; set; }
+            public byte[] Unknown3 { get; set; }
+
+            public bool IsPause()
+            {
+                return Type == 0x0209; 
+            }
+        }
+
+        private class Item
+        {
+            public TFString String { get; set; }
+            public List<Property> StringProperties { get; }
+            public byte Unknown { get; set; }
+
+            public Item()
+            {
+                StringProperties = new List<Property>();
+            }
+
+            public bool HasPauses()
+            {
+                return StringProperties.Any(x => x.Type == 0x0209);
+            }
+        }
+
+        private class StringGroup
+        {
+            public List<Item> Items { get; }
+            public byte[] Unknown { get; set; }
+
+            public StringGroup()
+            {
+                Items = new List<Item>();
+            }
+        }
+
+        private byte[] _magic;
+        private List<StringGroup> _stringGroups;
+        private byte[] _table1;
+        private List<TFString> _tableTitles;
+        private byte[] _remainder;
+
+        public MsgFileNew(string fileName) : base(fileName)
+        {
+            _stringGroups = new List<StringGroup>();
+            _tableTitles = new List<TFString>();
+        }
+
+        public override Endian Endianness => Endian.Big;
+        public override Encoding Encoding => Encoding.UTF8;
+
+        public override IList<TFString> Strings {
+            get
+            {
+                var result = new List<TFString>();
+
+                foreach (var s in _tableTitles)
+                {
+                    result.Add(s);
+                }
+
+                foreach (var g in _stringGroups)
+                {
+                    foreach (var i in g.Items)
+                    {
+                        result.Add(i.String);
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public override string FileType => "Msg";
+        
+        public override void Read(Stream s)
+        {
+            _magic = s.ReadBytes(3);
+
+            var stringGroupsCount = s.ReadByte();
+            var stringGroupStartPointer = s.ReadValueU32(Endianness);
+            var table1Pointer = s.ReadValueU32(Endianness);
+            var table1Count = s.ReadValueU16(Endianness);
+            var titlesCount = s.ReadValueU16(Endianness);
+            var titlesPointer = s.ReadValueU32(Endianness);
+            var remainderPointer = s.ReadValueU32(Endianness);
+
+            for (var i = 0; i < stringGroupsCount; i++)
+            {
+                s.Seek(stringGroupStartPointer + i * 16, SeekOrigin.Begin);
+                var strGroup = ReadStringGroup(s);
+                _stringGroups.Add(strGroup);
+            }
+
+            s.Seek(table1Pointer, SeekOrigin.Begin);
+            _table1 = s.ReadBytes(table1Count * 16);
+
+            for (var i = 0; i < titlesCount; i++)
+            {
+                s.Seek(titlesPointer + i * 4, SeekOrigin.Begin);
+                var title = ReadTitle(s);
+                _tableTitles.Add(title);
+            }
+
+            s.Seek(remainderPointer, SeekOrigin.Begin);
+            _remainder = s.ReadBytes((int) (s.Length - remainderPointer));
+        }
+
+        private StringGroup ReadStringGroup(Stream s)
+        {
+            var result = new StringGroup();
+
+            var groupStartPointer = s.ReadValueS64(Endianness);
+            var strCount = s.ReadValueU16(Endianness);
+            result.Unknown = s.ReadBytes(6);
+
+            for (var i = 0; i < strCount; i++)
+            {
+                s.Seek(groupStartPointer + (i * 12), SeekOrigin.Begin);
+                var item = ReadItem(s);
+                result.Items.Add(item);
+            }
+
+            return result;
+        }
+
+        private Item ReadItem(Stream s)
+        {
+            var result = new Item();
+            var strLength = s.ReadValueU16(Endianness);
+            var propCount = s.ReadByte();
+            result.Unknown = (byte)s.ReadByte();
+
+            var section = s.Position.ToString("X8");
+            var strPointer = s.ReadValueU32(Endianness);
+            var propPointer = s.ReadValueU32(Endianness);
+
+            s.Seek(strPointer, SeekOrigin.Begin);
+            result.String = GetString(s, section);
+
+            s.Seek(propPointer, SeekOrigin.Begin);
+            for (var i = 0; i < propCount; i++)
+            {
+                var prop = new Property
+                {
+                    Type = s.ReadValueU16(Endianness),
+                    Unknown1 = s.ReadBytes(2),
+                    Unknown2 = s.ReadBytes(2),
+                    Position = s.ReadValueU16(Endianness),
+                    Unknown3 = s.ReadBytes(8)
+                };
+                result.StringProperties.Add(prop);
+            }
+
+            return result;
+        }
+
+        private TFString ReadTitle(Stream s)
+        {
+            var pointer = s.ReadValueU32(Endianness);
+            s.Seek(pointer, SeekOrigin.Begin);
+            return GetString(s, "NAMES");
+        }
+
+        private TFString GetString(Stream s, string section)
+        {
+            var pos = s.Position;
+            var str = s.ReadStringZ(Encoding);
+            str = Yakuza0Project.ReadingReplacements(str);
+
+            var tfString = new TFString
+            {
+                FileId = Id,
+                Original = str,
+                Translation = str,
+                Offset = (int) pos,
+                Section = section,
+                Visible = !string.IsNullOrWhiteSpace(str),
+            };
+
+            return tfString;
+        }
+
+        public override void Save(string fileName, byte[] originalContent, IList<TFString> strings, ExportOptions options)
+        {
+            using (var fs = new FileStream(fileName, FileMode.Create))
+            {
+                fs.WriteBytes(_magic);
+                fs.WriteByte((byte)_stringGroups.Count);
+                fs.WriteValueS32(0x00000018, Endianness);
+                fs.WriteValueS32(0); //Puntero Tabla1
+                fs.WriteValueU16((ushort) (_table1.Length / 16), Endianness);
+                fs.WriteValueU16((ushort) _tableTitles.Count, Endianness);
+                fs.WriteValueS32(0); //Puntero Tabla Nombres
+                fs.WriteValueS32(0); //Puntero Resto
+
+                var totalStringsCount = 0;
+                var totalPropertiesCount = 0;
+                foreach (var sg in _stringGroups)
+                {
+                    totalStringsCount += sg.Items.Count;
+                    foreach (var item in sg.Items)
+                    {
+                        var str = FindString(item.String, strings);
+
+                        totalPropertiesCount += item.StringProperties.Count;
+                    }
+                }
+
+                var stringGroupPointer = 0x18;
+                var itemPointer = stringGroupPointer + 16 * _stringGroups.Count;
+                var propertiesPointer = itemPointer + 12 * totalStringsCount;
+                var stringsPointer = propertiesPointer + 16 * totalPropertiesCount;
+
+                for (var i = 0; i < _stringGroups.Count; i++)
+                {
+                    var sg = _stringGroups[i];
+
+                    fs.Seek(stringGroupPointer + i * 16, SeekOrigin.Begin);
+                    
+                    fs.WriteValueU64((ulong) itemPointer, Endianness);
+                    fs.WriteValueU16((ushort) sg.Items.Count, Endianness);
+                    fs.WriteBytes(sg.Unknown);
+
+                    for (var j = 0; j < sg.Items.Count; j++)
+                    {
+                        var item = sg.Items[j];
+                        var str = FindString(item.String, strings);
+                        var strLength = GetLength(str, options);
+
+                        fs.Seek(itemPointer + j * 12, SeekOrigin.Begin);
+                        fs.WriteValueU16((ushort) strLength, Endianness);
+                        fs.WriteByte((byte) item.StringProperties.Count);
+                        fs.WriteByte(item.Unknown);
+
+                        fs.WriteValueS32(stringsPointer, Endianness);
+                        fs.WriteValueS32(propertiesPointer, Endianness);
+
+                        fs.Seek(propertiesPointer, SeekOrigin.Begin);
+
+                        WriteProperties(fs, item, str);
+
+                        fs.Seek(stringsPointer, SeekOrigin.Begin);
+
+                        //WriteString(fs, str, options);
+
+                        propertiesPointer += item.StringProperties.Count * 16;
+                        stringsPointer = (int) fs.Position;
+                    }
+
+                    itemPointer += sg.Items.Count * 12;
+                }
+
+                fs.Seek(0, SeekOrigin.End);
+                while (fs.Position % 8 != 0)
+                {
+                    fs.WriteByte(0);
+                }
+
+                var table1Pointer = fs.Position;
+                fs.WriteBytes(_table1);
+
+                var table2Pointer = fs.Position;
+                
+                for (var i = 0; i < _tableTitles.Count; i++)
+                {
+                    fs.Seek(table2Pointer + i * 4, SeekOrigin.Begin);
+                    //remainderPointer = SaveTitle(_tableTitles[i], strings, options);
+                }
+
+
+                fs.Seek(0, SeekOrigin.End);
+                var remainderPointer = fs.Position;
+                fs.WriteBytes(_remainder);
+
+                /*fs.Seek(0x08, SeekOrigin.Begin);
+                fs.WriteValueS32((int)table1Pointer, Endianness);
+                fs.Seek(0x16, SeekOrigin.Begin);
+                fs.WriteValueS32((int)table2Pointer, Endianness);
+                fs.WriteValueS32((int)remainderPointer, Endianness);*/
+            }
+        }
+
+        public void Save(Stream s, byte[] originalContent, IList<TFString> strings, ExportOptions options)
+        {
+            
+        }
+
+        private void WriteProperties(Stream s, Item item, TFString tfStr)
+        {
+            if (tfStr.Original == tfStr.Translation)
+            {
+                foreach (var p in item.StringProperties)
+                {
+                    s.WriteValueU16(p.Type, Endianness);
+                    s.WriteBytes(p.Unknown1);
+                    s.WriteBytes(p.Unknown2);
+                    s.WriteValueU16(p.Position, Endianness);
+                    s.WriteBytes(p.Unknown3);
+                }
+            }
+            else
+            {
+                // Como la línea ha cambiado, las propiedades también
+                var newProperties = new List<Property>();
+                if (item.HasPauses())
+                {
+                    var newPauses = ParsePauses(tfStr.Translation);
+                    foreach (var property in item.StringProperties)
+                    {
+                        if (!property.IsPause())
+                        {
+                            newProperties.Add(property);
+                        }
+                    }
+                    newProperties.InsertRange(newProperties.Count - 1, newPauses);
+                }
+                else
+                {
+                    foreach (var property in item.StringProperties)
+                    {
+                        newProperties.Add(property);
+                    }
+                }
+
+                foreach (var p in newProperties)
+                {
+                    s.WriteValueU16(p.Type, Endianness);
+                    s.WriteBytes(p.Unknown1);
+                    s.WriteBytes(p.Unknown2);
+                    s.WriteValueU16(p.Position, Endianness);
+                    s.WriteBytes(p.Unknown3);
+                }
+            }
+        }
+
+        private static IList<Property> ParsePauses(string str)
+        {
+            var result = new List<Property>();
+            var shortPattern = @"(,\s)";
+            var longPattern = @"(\.\s|\!|\?)";
+
+            var temp = RemoveTags(str);
+
+            var matches = Regex.Matches(temp, $@"{shortPattern}|{longPattern}");
+
+            foreach (Match match in matches)
+            {
+                var duration = Regex.IsMatch(match.Value, shortPattern) ? 0x0a : 0x14;
+
+                var p = new Property
+                {
+                    Type = 0x0209,
+                    Unknown1 = new byte[] {0x00, (byte) duration},
+                    Unknown2 = new byte[] {0x00, 0x00},
+                    Position = (ushort) (match.Index + 1),
+                    Unknown3 = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+                };
+
+                result.Add(p);
+            }
+
+            return result;
+        }
+
+        private static TFString FindString(TFString original, IList<TFString> strings)
+        {
+            return strings.FirstOrDefault(s => s.Offset == original.Offset);
+        }
+
+        private int GetLength(TFString tfStr, ExportOptions options)
+        {
+            if (tfStr.Original == tfStr.Translation)
+            {
+                var temp = tfStr.Original.Replace("\\r\\n", "\r\n");
+
+                return temp.GetLength(Encoding);
+            }
+            else
+            {
+                var temp = tfStr.Translation.Replace("\\r\\n", "\r\n");
+
+                return temp.GetLength(options.SelectedEncoding);
+            }
+        }
+
+        private static string RemoveTags(string strOriginal)
+        {
+            var temp = strOriginal.Replace("\\r\\n", " ");
+            temp = temp.Replace("\\n", " ");
+            temp = Regex.Replace(temp, @"<Color[^>]*>", string.Empty);
+            return Regex.Replace(temp, @"<[^>]*>", " ");
+        }
+    }
+}
